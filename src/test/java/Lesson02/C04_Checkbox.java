5eb221ad1b3949380bca6dfc5b532e4b8f5da0b6
alitu <alym11731996@gmail.com>
2024-02-16 23:42:24 +0300

    Checkbox

--- src/test/java/Lesson02/C04_Checkbox.java
+++ src/test/java/Lesson02/C04_Checkbox.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C04_CheckBox {
+public class C04_Checkbox {
     WebDriver driver; //class seviyesinde kullanılsın diye before un üstüne class ın içine tanınmlıyorum! (scope)
     @Before
     public void setUp() {
