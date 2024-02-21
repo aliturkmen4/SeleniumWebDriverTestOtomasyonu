06327e725492201ac2bd51c078243f1407552866
alitu <alym11731996@gmail.com>
2024-02-21 17:40:55 +0300

    Getting screenshot

---
+++ src/test/java/Lesson08/C01_GetScreenshot.java
@@ -0,0 +1,23 @@
+package Lesson08;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.TestBaseBeforeAfter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class C01_GetScreenshot extends TestBaseBeforeAfter {
+    @Test
+
+    public void test() throws IOException {
+        //bestbuy sayfasına gidelim tum sayfanın resmini alalim
+        driver.navigate().to("https://www.bestbuy.com");
+
+        TakesScreenshot screenshot=(TakesScreenshot) driver;
+        File tumSayfa=screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(tumSayfa, new File("target/ekranGoruntusu/AllPage"+tarih+".jpeg"));
+    }
+}
