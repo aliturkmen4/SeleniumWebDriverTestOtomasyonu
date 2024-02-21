25c61060672f41469b417922df2203b5a7c53ede
alitu <alym11731996@gmail.com>
2024-02-21 17:40:41 +0300

    date format added

--- src/test/java/utilities/TestBaseBeforeAfter.java
+++ src/test/java/utilities/TestBaseBeforeAfter.java
@@ -7,16 +7,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TestBaseBeforeAfter {
 
     protected WebDriver driver; //extend edebilmek için protected eklemem lazım!
+
+    protected String tarih;
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //10 sn içinde yüklenirse testim okey!
+
+        LocalDateTime date=LocalDateTime.now();
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("YYYYMMDDHHmmss");
+        tarih=date.format(formatter);
     }
     @After
 
